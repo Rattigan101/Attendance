db463d638baf40e8e0d532ea83bc889c138d0ee4
Rattigan101 <shari_rattigan@ymail.com>
2022-10-30 15:37:07 -0500

    Initial commit

---
+++ success.php
@@ -0,0 +1,79 @@
+<?php
+    $title = 'Success';
+    require_once 'includes/header.php';
+    require_once 'db/db_con.php'; 
+    require_once 'sendemail.php';  
+
+    if(isset($_POST['submit'])){
+        // extract values from the $_POST array
+        $fname = $_POST['firstname'];
+        $lname = $_POST['lastname'];
+        $dob = $_POST['dob'];
+        $email = $_POST['email'];
+        $contact = $_POST['phone'];
+        $specialty = $_POST['specialty'];
+
+        $orig_file = $_FILES["avatar"]["tmp_name"];
+        $ext = pathinfo($_FILES["avatar"]["name"], PATHINFO_EXTENSION);
+        $target_dir = "uploads/";
+        $destination = "$target_dir$contact . $ext";
+        move_uploaded_file($orig_file,$destination);
+
+        // call function to insert and track if successful or not
+        $isSuccess = $crud->insertAttendees($fname, $lname, $dob, $email, $contact, $specialty, $destination);
+        $specialtyName = $crud->getSpecialtyById($specialty);
+
+        if($isSuccess){
+            SendEmail::SendMail($email, 'Welcome to IT Conference 2020', 'You have successfully registered for this year\'s IT Conference');
+            include 'includes/successmessage.php';
+        }
+        else{
+            include 'includes/errormessage.php';
+        }
+    }
+?> 
+    <!-- This prints out values using method = "get" -->
+    <!--
+    <div class="card" style="width: 18rem;">
+        <div class="card-body">
+            <h5 class="card-title">
+                <?php //echo $_GET['firstname'] . ' ' . $_GET['lastname']; ?>
+            </h5>
+            <h6 class="card-subtitle mb-2 text-muted">
+                <?php //echo $_GET['specialty']; ?>
+            </h6>
+            <p class="card-text">
+                Date of Birth: <?php //echo $_GET['dob']; ?>
+            </p>
+            <p class="card-text">
+                Email Address: <?php //echo $_GET['email']; ?>
+            </p>
+            <p class="card-text">
+                Telephone Number<?php //echo $_GET['phone']; ?>
+            </p>
+        </div>
+    </div> -->
+    <!-- This prints out values using method = "post" -->
+    <img src="<?php echo $destination; ?>" class="rounded-circle" style="width: 20%; height: 20%" />
+    <div class="card" style="width: 18rem;">
+        <div class="card-body">
+            <h5 class="card-title">
+                <?php echo $_POST['firstname'] . ' ' . $_POST['lastname']; ?>
+            </h5>
+            <h6 class="card-subtitle mb-2 text-muted">
+                <?php echo $specialtyName['name']; ?>
+            </h6>
+            <p class="card-text">
+                Date of Birth: <?php echo $_POST['dob']; ?>
+            </p>
+            <p class="card-text">
+                Email Address: <?php echo $_POST['email']; ?>
+            </p>
+            <p class="card-text">
+                Telephone Number: <?php echo $_POST['phone']; ?>
+            </p>
+        </div>
+    </div>   
+
+<br>
+<br>
---
+++ viewrecords.php
@@ -0,0 +1,37 @@
+<?php
+    $title = 'View Records';
+    require_once 'includes/header.php';
+    require_once 'includes/auth_check.php';
+    require_once 'db/db_con.php'; 
+
+    // Get all attendees
+    $result = $crud->getAttendees();
+?>
+    <table class="table">
+        <tr>
+            <th>No.</th>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Specialty</th>
+            <th>Actions</th>
+        </tr>
+        <?php while($r = $result->fetch(PDO::FETCH_ASSOC)) { ?>
+            <tr>
+                <td><?php echo $r['attendee_id']?></td>
+                <td><?php echo $r['firstname']?></td>
+                <td><?php echo $r['lastname']?></td>
+                <td><?php echo $r['name']?></td>
+                <td>
+                    <a href="view.php?id=<?php echo $r['attendee_id']?>" class="btn btn-info">View</a>
+                    <a href="edit.php?id=<?php echo $r['attendee_id']?>" class="btn btn-primary">Edit</a>
+                    <a onclick="return confirm('Are you sure you to delete this record?');"
+                    href="delete.php?id=<?php echo $r['attendee_id']?>" class="btn btn-danger">Delete</a>
+                </td>
+            </tr>
+        <?php }?>
+    </table>
+<br>
+<br>
+<br>
+<br>
+<?php require_once 'includes/footer.php'; ?>
